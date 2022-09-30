6a055b662438a107de011d4b6a13550ef5b3349a
digi@Lib <69793088+MkumarDigilib@users.noreply.github.com>
2022-10-01 00:57:31 +0530

    Add files via upload

---
+++ admin_validation.js
@@ -0,0 +1,19 @@
+let value2=''
+function generateotp(){
+    let val_random = Math.floor(1000 + Math.random() * 9000);
+                random_val = val_random.toString();
+                document.getElementById("otp").value=random_val
+                value2=random_val
+}
+
+
+function check(){
+    let enteredotp=document.getElementById('otp').value
+    if(enteredotp == value2){
+     window.location.href="update.html"
+    }
+    else{
+        alert("Wrong OTP (NOTE: Only admin has the access to create post)")
+    }
+    console.log(value2)
+}

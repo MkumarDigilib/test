e4b0227089dca0a054c435d186a51147c309bd6a
digi@Lib <69793088+MkumarDigilib@users.noreply.github.com>
2022-10-06 21:10:54 +0530

    Update postlogic.js

--- postlogic.js
+++ postlogic.js
@@ -1,18 +1,44 @@
 var color=['brown','red','blue','warning','pink']
-var num_sec=432000
+var num_sec=259200
 var tag_name=''
 var file_data
+var link=[],Type=[],Title=[],Detail=[],image=[],tmt=[]
     fetch('https://script.google.com/macros/s/AKfycbzf2QLs_qOM1BZo23tvWCtULNuAotQtRxG-uAS6sjoN6a_eJ0Q-ixdG2Fu3q_THdEkN/exec')
      .then((data)=>{return data.json();
      }).then((completedata)=>{
         console.log(completedata)
-        let data1="";
+        
         completedata.map((values)=>{
-          function getRandomInt(max) {
+             link.push(values.Link)
+             Type.push(values.type)
+             Title.push(values.titles)
+             Detail.push(values.detail)
+             image.push(values.imageurl)
+             //tag.push(values.tag_name)
+             tmt.push(values.time)
+
+        });
+       //document.getElementById("arrayoflinks").innerHTML=data1; 
+       temp();        
+     }).catch((err)=>{
+      console.log(err);
+     })
+
+
+function create_post(){
+  document.getElementById('postlink').href="adminvalidation.html"
+}
+
+function temp(){
+    s=(Title.length)-1
+    let data1="";
+    for(let i=s;i>=0;i--){
+
+        function getRandomInt(max) {
             return Math.floor(Math.random() * max);
           }
-
-          post_time=values.time
+    
+          post_time=tmt[i]
           const now = new Date()  
           const utcMilllisecondsSinceEpoch = now.getTime() + (now.getTimezoneOffset() * 60 * 1000)  
           const utcSecondsSinceEpoch = Math.round(utcMilllisecondsSinceEpoch / 1000)
@@ -26,35 +52,33 @@ var file_data
           
           var indexofarray=getRandomInt(5)
           var colorvalue=color[indexofarray]
-          if(values.Link != ''){
+          if(link[i] != ''){
          data1+=`<div class="card" id="cardcolor">
          <h6 id="tag" name="tag" style=" color:white;  background-color:blue; margin-left:90% ; position:absolute;"  >${tag_name}</h6>
          <div class="card__header"> 
-           <img style="height:100%; display:inline-block; width: 50%; margin-left:25%;" src="${values.imageurl}" alt="card__image" class="card-img-top" >
+           <img style="height:100%; display:inline-block; width: 50%; margin-left:25%;" src="${image[i]}" alt="card__image" class="card-img-top" >
          </div>
          <div class="card__body">
-           <span class="tag tag-${colorvalue}">${values.type}</span>
-           <h4>${values.titles}</h4>
-           <p>${values.detail}</p>
+           <span class="tag tag-${colorvalue}">${Type[i]}</span>
+           <h4>${Title[i]}</h4>
+           <p>${Detail[i]}</p>
          </div>
          <div class="card__footer">
            <div class="user">
              
              <div class="user__info">
-               <span class="tag tag-green"><a target="_blank" style="text-decoration: none; color: white; font-size:15px;"href="${values.Link}">Click</a></span>
+               <span class="tag tag-green"><a target="_blank" style="text-decoration: none; color: white; font-size:15px;"href="${link[i]}">Click</a></span>
                
              </div>
            </div>
          </div>
        </div>`
-;}
-        });
-       document.getElementById("arrayoflinks").innerHTML=data1;         
-     }).catch((err)=>{
-      console.log(err);
-     })
+    ;}
 
+     
 
-function create_post(){
-  document.getElementById('postlink').href="adminvalidation.html"
+    }
+
+    document.getElementById("arrayoflinks").innerHTML=data1;
+    
 }
